22411bb9ad6d1530a9c8400343abc1d53adec848
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-17 00:20:24 +0530

    component rendering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./index.css";
+import About from "./Components/About";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <>
+      <Switch>
+        <Route exact path="/">
+          This is home page
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
-
-      <Route>This is 404 page</Route>
-    </Switch>
+        <Route exact path="/starred">
+          This is starred
+        </Route>
+        {/* component rendering */}
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route>This is 404 page</Route>
+      </Switch>
+      {/* <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch> */}
+    </>
   );
 }
 
