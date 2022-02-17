8c3c76ddc56609d34d2a75951db0a094f1776125
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-17 15:33:50 +0530

    component and layout work

--- src/Components/MainPageLayout.jsx
+++ src/Components/MainPageLayout.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Title from "./Title";
 
-export default function MainPageLayout() {
+export default function MainPageLayout({ children }) {
   return (
-    <div>
+    <div className="app">
+      <Title title="Box Office" subtitle="Looking for a movie or an actor" />
       <Navbar />
+      {children}
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainPageLayout from "../Components/MainPageLayout";
+import Navbar from "../Components/Navbar";
 import "../index.css";
 
 export default function Home() {
