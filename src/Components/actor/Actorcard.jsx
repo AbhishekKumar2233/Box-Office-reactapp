e97c301bc2a7d2b7fb86fe007ef732bcf77a6a24
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-18 15:44:22 +0530

    actor card completed

--- src/Components/actor/Actorcard.jsx
+++ src/Components/actor/Actorcard.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-export default function Actorcard({ id, name }) {
+export default function Actorcard({
+  id,
+  name,
+  img,
+  gender,
+  country,
+  birthday
+}) {
   return (
     <div>
+      <img src={img} alt={name} />
       <h2>{name}</h2>
+      <h3>{gender}</h3>
+      <p>{country ? `Comes from ${country}` : "No Country Known"}</p>
+      <p>{birthday ? <p>Born {birthday}</p> : null}</p>
     </div>
   );
 }
--- src/Components/actor/Actorgrid.jsx
+++ src/Components/actor/Actorgrid.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Actorcard from "./Actorcard";
+// import img from '../../images/img.png';
 
 export default function Actorgrid({ data }) {
   return (
     <div>
       {data.map(({ person }) => (
-        <Actorcard key={person.id} id={person.id} name={person.name} />
+        <Actorcard
+          key={person.id}
+          id={person.id}
+          name={person.name}
+          gender={person.gender}
+          country={person.country ? person.country.name : null}
+          img={person.image ? person.image.medium : null}
+        />
       ))}
     </div>
   );
