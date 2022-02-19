8a98e255ad9c51505d57ccd465cc3dc4e24edf86
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-19 23:10:53 +0530

    Params used here

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function Show() {
-  return <h1>This is Show page</h1>;
+  //useParams is hook of react-router-dom
+  //used to get value form url
+  const params = useParams();
+  console.log(params);
+
+  return <h1>This is Show page </h1>;
 }
