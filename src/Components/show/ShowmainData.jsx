441308cd81a408116b9d0e00ee55956fa8d011c4
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-21 00:09:33 +0530

    Show main details data fetched

--- src/Components/show/ShowmainData.jsx
+++ src/Components/show/ShowmainData.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function ShowmainData() {
-  return <h1>Show Main Data</h1>;
+export default function ShowmainData({ name, rating, summary, tags, image }) {
+  return (
+    <div>
+      <img src={image ? image.original : null} alt={name} />
+      <div>
+        <h1>{name}</h1>
+        <div>
+          {/* <Star /> */}
+          <span> {rating.average || "N/A"}</span>
+        </div>
+      </div>
+      <div dangerouslySetInnerHTML={{ _html: summary }}>
+        <div>
+          Tags:{""}
+          <div>
+            {tags.map((tag, i) => {
+              <span key={i}>{tag}</span>;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
