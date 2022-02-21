6a46dca4fb41290f3d1102f112a87cece697b6b2
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-21 23:50:27 +0530

    some change in season

--- src/Components/show/Seasons.jsx
+++ src/Components/show/Seasons.jsx
@@ -12,6 +12,18 @@ export default function Seasons({ seasons }) {
           {seasons.reduce((acc, season) => acc + seasons.episodeOrder, 0)}
         </span>
       </p>
+      <div>
+        {seasons.map((season) => (
+          <div key={season.id}>
+            <div>
+              <p>Seasons {season.number}</p>
+              <p>
+                Episodes: <span>{season.episodeOrder}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
