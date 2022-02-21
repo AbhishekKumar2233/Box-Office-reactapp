fcb1be1194f791fdff419443c2a927a41140740e
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-21 23:39:37 +0530

    finally show page done

--- src/Components/show/Seasons.jsx
+++ src/Components/show/Seasons.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function Seasons() {
-  return <h1>Hyy Seasons</h1>;
+export default function Seasons({ seasons }) {
+  return (
+    <div>
+      <p>
+        Seasons in total: <span>{seasons.length} </span>
+      </p>
+      <p>
+        Episodes in total {""}
+        <span>
+          {seasons.reduce((acc, season) => acc + seasons.episodeOrder, 0)}
+        </span>
+      </p>
+    </div>
+  );
 }
