d1be4b45e354531f72248d097079a72d23374bd8
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-23 15:59:04 +0530

    Hook for use browsers local storage

---
+++ src/misc/custom-hook.js
@@ -0,0 +1,32 @@
+import { useReducer, useEffect } from "react";
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD':{
+      return [...prevState,action.showID];
+    }
+    case 'REMOVE':{
+      return prevState.filter(showID => showID !== action.showID);
+    }
+    default:
+      return prevState;
+  }
+}
+
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+
+    //initial func
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+  return [state, dispatch];
+}
+
+export function useShows(key = "shows") {
+  return usePersistedReducer(showsReducer, [], key);
+}
