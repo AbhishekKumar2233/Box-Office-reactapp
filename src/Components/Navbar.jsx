bad322608662f740f9a550736daffc8ef2c65e15
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-23 16:57:57 +0530

    error solved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Links = [
   { to: "/", name: "Home" },
-  { to: "about", name: "About" }
+  { to: "/starred", name: "Starred" }
 ];
 
 export default function Navbar() {
