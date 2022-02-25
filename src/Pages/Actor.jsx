48c0871c5606dd387cdfd53b387e176f65d57d65
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-26 00:27:01 +0530

    error and loading handler in actor page

--- src/Pages/Actor.jsx
+++ src/Pages/Actor.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useActor } from "../misc/custom-hook";
 import ActorMainData from "../Components/actor/ActorMainData";
 
 export default function Actor() {
   //useParams is hook of react-router-dom
   //used to get value form url
-  const params = useParams();
-  console.log(params);
+  const { id } = useParams();
+
+  const { show, isLoading, error } = useActor(id);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>Error{error}</div>;
+  }
 
   return (
     <div>
