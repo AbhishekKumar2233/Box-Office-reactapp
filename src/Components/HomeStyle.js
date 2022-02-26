339f02ba23a28c7018549250c5592005c53cc4f0
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-26 23:41:26 +0530

    style for search and home page

---
+++ src/Components/HomeStyle.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SearchInput = styled.div`
+  display: block;
+  width: 200px;
+  margin: auto;
+  outline: none;
+  padding: 13px 15px;
+  border: 1px solid #dbdbdb;
+  box-shadow: 0px 0px 10px 0px rgba(219, 219, 218, 0.5);
+  font-size: 14px;
+  border-radius: 12px;
+  color: #8d8d8d;
+  &::placeholder {
+    font-weight: 300;
+    color: #8d8d8d;
+  }
+`;
